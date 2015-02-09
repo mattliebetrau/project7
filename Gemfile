2a91e6f33b9717dcfcd475e979c4f7ba90060382
Matt Liebetrau <matt.liebetrau@gmail.com>
2015-02-09 13:08:37 -0500

    added control_for

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'rack', '1.5.2'
+gem 'sinatra-contrib'
--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 # app.rb
 require 'sinatra'
+require "sinatra/content_for"
 
 class MyWebApp < Sinatra::Base
+  helpers Sinatra::ContentFor
   get '/' do
   	last_modified Time.now
     erb :"index#{ rand(3) + 1 }"
   end
 end
+
