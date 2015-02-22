82a008a7babf43858e41439a1bf72af67ef9a836
Matt Liebetrau <matt.liebetrau@gmail.com>
2015-02-22 17:20:47 -0500

    added holiday api and styled

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 # app.rb
 require 'sinatra'
-require "sinatra/content_for"
+require 'sinatra/content_for'
+require 'cat_api'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
   helpers Sinatra::ContentFor
   get '/' do
   	last_modified Time.now
-    erb :"index#{ rand(3) + 1 }"
+    @holidays_year_current = HolidApi.get(year: Time.now.year)
+    @holidays_month_current = HolidApi.get(month: Time.now.month)
+    @holidays_matt = HolidApi.get(year: 1992, month: 8)
+    erb :index
   end
 end
+
 
