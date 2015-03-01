75fc67f0448fa745987485865c13185ddec9bc52
Matt Liebetrau <matt.liebetrau@gmail.com>
2015-03-01 14:31:28 -0500

    first commit using forms

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 # app.rb
 require 'sinatra'
 require 'sinatra/content_for'
-require 'cat_api'
 require 'holidapi'
 
 class MyWebApp < Sinatra::Base
   helpers Sinatra::ContentFor
   get '/' do
-  	last_modified Time.now
-    @holidays_year_current = HolidApi.get(year: Time.now.year)
-    @holidays_month_current = HolidApi.get(month: Time.now.month)
-    @holidays_matt = HolidApi.get(year: 1992, month: 8)
+    params['country'] ||= 'us'
+    params['year'] ||= Time.now.year
+    params['month'] ||= Time.now.month
+    @holidays = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
     erb :index
   end
 end
